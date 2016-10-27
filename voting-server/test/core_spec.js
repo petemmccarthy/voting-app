337773788b0666634b6bc92045a1c0afbad8b8c1
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-27 16:56:04 +0100

    entries fnct and next fnct test added

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
@@ -13,6 +13,23 @@ describe('application logic', () => {
       const nextState = setEntries(state, entries);
       expect(nextState).to.equal(Map({
         entries: List.of('Trainspotting', '28 days later');
+      }));
+    });
+
+  });
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Trainspotting', '28 days later', 'Sunshine')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Trainspotting', '28 days later');
+        });
+        entries: List.of('Sunshine');
       }));
     });
 
