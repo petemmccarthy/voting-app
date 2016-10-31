df731610a5824e75701b2290714f776f55b50bea
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-10-31 17:12:32 +0000

    reducer tests

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -1,7 +1,7 @@
 import {Map, fromJS} from 'immutable';
 import {expect} from 'chai';
 
-import reducer from '../src/reducerr.js';
+import reducer from '../src/reducer.js';
 
 describe('reducer', () => {
 
@@ -12,6 +12,40 @@ describe('reducer', () => {
 
     expect(nextState).to.equal(fromJS({
       entries: ['Trainspotting']
+    }));
+  });
+
+  it('handles NEXT', () => {
+    const initialState = fromJS({
+      entries: ['Trainspotting', '28 days later']
+    });
+    const action = {type: 'NEXT'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 days later']
+      },
+      entries: []
+    }));
+  });
+
+  it('handles VOTE', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 days later']
+      },
+      entries: []
+    });
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    const nextState = reducer(initialState, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 days later'],
+        tally: {'Trainspotting': 1}
+      },
+      entries: []
     }));
   });
 
