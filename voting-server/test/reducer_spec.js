ef31ca726c27cf5828abcf8da0ff554df8a85c3f
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-11-01 10:57:18 +0000

    test passing

--- voting-server/test/reducer_spec.js
+++ voting-server/test/reducer_spec.js
@@ -50,7 +50,7 @@ describe('reducer', () => {
   });
 
   it('has an initial state', () => {
-    const action = {type: 'SET_ENTRIES', entries: 'Trainspotting'};
+    const action = {type: 'SET_ENTRIES', entries: ['Trainspotting']};
     const nextState = reducer(undefined, action);
     expect(nextState).to.equal(fromJS({
       entries: ['Trainspotting']
