c80eb71386e767dd59e6f5ac65127f3141981519
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-11-03 07:17:12 +0000

    has voted property

--- voting-client/src/components/Voting.jsx
+++ voting-client/src/components/Voting.jsx
@@ -4,6 +4,9 @@ export default React.createClass({
   getPair: function() {
     return this.props.pair || [];
   },
+  isDisabled: function() {
+    return !!this.props.hasVoted;
+  },
   render: function() {
     return <div className="voting">
       {this.getPair().map(entry =>
