79a1b2984c43da15f0536c53a02aec22149fc977
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-11-04 16:35:26 +0000

    winner now displayed

--- voting-client/src/components/Results.jsx
+++ voting-client/src/components/Results.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import Winner from './Winner';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -14,24 +15,26 @@ export default React.createClass({
     return 0;
   },
   render: function() {
-    return <div className="results">
-      <div className="tally">
-        {this.getPair().map(entry =>
-          <div key={entry} className="entry">
-            <h1>{entry}</h1>
-            <div class="voteCount">
-              {this.getVotes(entry)}
+    return this.props.winner?
+      <Winner ref="winner" winner={this.props.winner} /> :
+      <div className="results">
+        <div className="tally">
+          {this.getPair().map(entry =>
+            <div key={entry} className="entry">
+              <h1>{entry}</h1>
+              <div className="voteCount">
+                {this.getVotes(entry)}
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-      <div className="management">
-        <button ref="next"
-                className="next"
-                onClick={this.props.next}>
-          Next
-        </button>
-      </div>
-    </div>;
+          )}
+        </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+            Next
+          </button>
+        </div>
+      </div>;
   }
 });
