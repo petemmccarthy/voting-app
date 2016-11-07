58d45a4f93fd478b8959f768e2388d8d9db610bd
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-11-07 14:43:26 +0000

    redux

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
 import Results from './components/Results';
 
-const store = createStore(reducer;
+const store = createStore(reducer);
 store.dispatch({
   type: 'SET_STATE',
   state: {
     vote: {
-      vote: ['Sunshine', '28 Days Later'],
+      pair: ['Sunshine', '28 Days Later'],
       tally: {Sunshine: 2}
     }
   }
@@ -23,6 +25,8 @@ const routes = <Route component={App}>
 </Route>;
 
 ReactDOM.render(
-  <Router history={hashHistory}>{routes}</Router>,
+  <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>,
+  </Provider>,
   document.getElementById('app')
 );
