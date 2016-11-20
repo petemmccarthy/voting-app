2712555855f6694907a0d5ea1d9aed41dadd19e2
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-11-20 19:29:41 +0000

    vote handling test

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -67,4 +67,24 @@ describe('reducer', () => {
     }));
   });
 
+  it('handles VOTE by setin hasVoted', () => {
+    const state = from JS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    });
+
+    const action = {type: 'VOTE', entry: 'Trainspotting'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      hasVoted: 'Trainspotting'
+    }));
+  });
+
 });
