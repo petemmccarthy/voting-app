d420d80c6a533bea28bd8758273d7d7051a4a95f
Peter McCarthy <petermccarthy49@yahoo.co.uk>
2016-11-20 19:34:29 +0000

    invalid vote test

--- voting-client/test/reducer_spec.js
+++ voting-client/test/reducer_spec.js
@@ -87,4 +87,22 @@ describe('reducer', () => {
     }));
   });
 
+  it('does not set hasVoted for VOTE on invalid entry', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    });
+    const action = {type: 'VOTE', entry: 'Sunshine'};
+    const nextState = reducer(state, action);
+
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    }));
+  });
+
 });
